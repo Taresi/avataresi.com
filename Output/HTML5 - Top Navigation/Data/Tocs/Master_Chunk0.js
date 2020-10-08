1f6e58f77913800c09f948e143a103d802924fee
Taresi <ataresi@myseneca.ca>
2020-10-08 18:30:37 -0400

    MadCap Oct 08

---
+++ Output/HTML5 - Top Navigation/Data/Tocs/Master_Chunk0.js
@@ -0,0 +1,1 @@
+define({'/Content/AboutMe.htm':{i:[0],t:['About Me'],b:['']},'/Content/Contact Me.htm':{i:[3],t:['Contact Me'],b:['']},'/Content/Portfolio/Portfolio.htm':{i:[1],t:['Portfolio'],b:['']},'/Content/TechComm Authoring Tools.htm':{i:[2],t:['TechComm Authoring Tools'],b:['']}});
